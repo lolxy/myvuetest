e7770164c83a9204c101938ceef31302dfca360a
lolxy <lolxy@users.noreply.github.com>
2017-10-06 17:53:03 +0800

    新增我的第一个页面

---
+++ src/pages/FirstPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>这是我的第一个页面</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'firstpage',
+  data () {
+    return {
+      msg: '欢迎使用vue'
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import FirstPage from '@/pages/FirstPage'
 
 Vue.use(Router)
+
+/**
+ * The routes
+ *
+ * @type {object} The routes
+ */
 
 export default new Router({
   routes: [
@@ -10,6 +17,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/firstpage',
+      name: 'FirstPage',
+      component: FirstPage
     }
   ]
 })
